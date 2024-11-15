9671c0069c11a2cfb634d34dad5fc8696806bfdb
danilo-dps <danilodpsnilo@outlook.com>
2024-11-15 01:43:43 -0300

    Project created

---
+++ src/test/java/com/danilodps/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danilodps.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
