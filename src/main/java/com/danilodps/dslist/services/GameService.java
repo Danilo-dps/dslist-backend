fea1fe3a3dfced457ccc50b5f9a3b5784aad5aaa
danilo-dps <danilodpsnilo@outlook.com>
2024-11-15 10:52:46 -0300

    GET game by id

--- src/main/java/com/danilodps/dslist/services/GameService.java
+++ src/main/java/com/danilodps/dslist/services/GameService.java
@@ -4,7 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import com.danilodps.dslist.dto.GameDTO;
 import com.danilodps.dslist.dto.GameMinDTO;
 import com.danilodps.dslist.entities.Game;
 import com.danilodps.dslist.repositories.GameRepository;
@@ -15,6 +18,13 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)
+	public GameDTO findById(@PathVariable Long listId) {
+		Game result = gameRepository.findById(listId).get();
+		return new GameDTO(result);
+	}
+	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
 		return result.stream().map(GameMinDTO::new).toList();
